902f070e4aac115847e94100e1b036d93f6eefd6
LeonRivera <leonriv2@gmail.com>
2022-05-05 13:11:07 -0500

    05052022 | LeonRivera | Readme updated

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -8,57 +8,36 @@ import { PrimeNumberService } from '../prime-number.service';
   styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
-  constructor(private primeNumberService:PrimeNumberService) {}
+  constructor(private primeNumberService: PrimeNumberService) {}
 
   ngOnInit(): void {}
 
   strValue: string = '';
 
-  valor:number = 0;
+  valor: number = 0;
 
-  valorColores:string[] = [];
+  valorColores: string[] = [];
 
-  valoresPrimos:PrimeNumber[] = [];
-
-  
- 
+  valoresPrimos: PrimeNumber[] = [];
 
   public inputValue(event: Event): void {
     this.strValue = (<HTMLInputElement>event.target).value;
     this.valor = Number(this.strValue);
   }
 
-  public calcularMultiplos(): void{
-
-
+  public calcularMultiplos(): void {
     for (let i = 0; i <= this.valor; i++) {
-
-        
-
-
-        if(i % 3 === 0 && i !== 0){
-
-            
-
-            this.valoresPrimos.push(new PrimeNumber("green", i));
-
-        }
-        else if(i % 5 === 0 && i !== 0){
-
-          this.valoresPrimos.push(new PrimeNumber("red", i));
-        }
-        else if(i % 7 === 0 && i !== 0){
-
-          this.valoresPrimos.push(new PrimeNumber("blue", i));
-        }else{
-
-          this.valoresPrimos.push(new PrimeNumber("black", i));
-        }
+      if (i % 3 === 0 && i !== 0) {
+        this.valoresPrimos.push(new PrimeNumber('green', i));
+      } else if (i % 5 === 0 && i !== 0) {
+        this.valoresPrimos.push(new PrimeNumber('red', i));
+      } else if (i % 7 === 0 && i !== 0) {
+        this.valoresPrimos.push(new PrimeNumber('blue', i));
+      } else {
+        this.valoresPrimos.push(new PrimeNumber('black', i));
+      }
     }
 
-
     this.primeNumberService.savePrimeNumbers(this.valoresPrimos);
-
   }
-
 }
